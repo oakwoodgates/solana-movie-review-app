e4c979b41af207c6f6d45518eb154d6428935bb5
oakwoodgates <wpguru4u@gmail.com>
2023-02-22 22:56:01 -0500

    output the data

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,14 +1,32 @@
 import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { Movie } from '../models/Movie'
+import * as web3 from '@solana/web3.js'
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
 export const MovieList: FC = () => {
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [movies, setMovies] = useState<Movie[]>([])
 
     useEffect(() => {
-        setMovies(Movie.mocks)
+        connection.getProgramAccounts(new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID))
+        .then(async (accounts) => {
+            console.log('accounts', accounts)
+            const movies: Movie[] = accounts.reduce((accum: Movie[], { pubkey, account }) => {
+
+                // Try to extract movie item from account data
+                const movie = Movie.deserialize(account.data)
+
+                // If the account does not have a review, movie will be null
+                if (!movie) {
+                    return accum
+                }
+
+                return [...accum, movie]
+            }, [])
+            setMovies(movies)
+        })
     }, [])
     
     return (
